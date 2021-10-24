96b76808ac08225b70b548d836e657378ecd16de
eojinlee314 <84307340+eojinlee314@users.noreply.github.com>
2021-10-24 21:05:27 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,24 @@
+#define PIN7 7
+
+void setup() {
+  pinMode(PIN7, OUTPUT);
+  digitalWrite(PIN7, LOW);
+  delay(1000);
+}
+
+void loop() {
+  int i = 0;
+  while(i<5) {
+  digitalWrite(PIN7, HIGH);
+  delay(200);
+
+  digitalWrite(PIN7, LOW);
+  delay(200);
+
+  i ++;
+  }
+
+  digitalWrite(PIN7, HIGH);
+
+  while(1) {}
+}
